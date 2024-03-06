5fd0cf1c3bd59762c8d252335ae973a053a106e3
Nathan Gutiérrez Gil <nathangut98@gmail.com>
2024-03-06 15:34:16 +0100

    install django-djdatabase

--- crud_django/build.sh
+++ crud_django/build.sh
@@ -8,6 +8,7 @@ pip install django
 pip install dj-database-url
 pip install gunicorn
 pip install psycopg2
+pip install whitenoise
 # Convert static asset files
 python manage.py collectstatic --no-input
 
