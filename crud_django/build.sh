1cc214bfab687e561c23ea9d3efd2f078859046c
Nathan Gutiérrez Gil <nathangut98@gmail.com>
2024-03-06 15:29:00 +0100

    install django-djdatabase

--- crud_django/build.sh
+++ crud_django/build.sh
@@ -5,6 +5,7 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 #pip install -r requirements.txt
 pip install django
+pip install dj-database-url
 pip install gunicorn
 # Convert static asset files
 python manage.py collectstatic --no-input
